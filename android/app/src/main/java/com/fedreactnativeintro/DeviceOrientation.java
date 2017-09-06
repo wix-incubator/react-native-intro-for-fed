28e6696642baac984402a5d866e19b431755ab8d
ofird <ofirdagan@gmail.com>
2017-09-06 18:35:42 +0300

    feat(app): add orientation - Native Module!!

---
+++ android/app/src/main/java/com/fedreactnativeintro/DeviceOrientation.java
@@ -0,0 +1,34 @@
+package com.fedreactnativeintro;
+
+import android.app.Activity;
+import android.support.v7.widget.LinearLayoutCompat;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import static android.content.res.Configuration.ORIENTATION_LANDSCAPE;
+
+/**
+ * Created by ofird on 06/09/2017.
+ */
+
+public class DeviceOrientation extends ReactContextBaseJavaModule {
+
+    public DeviceOrientation(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "Orientation";
+    }
+
+    @ReactMethod
+    public void getCurrent(Promise promise) {
+        int orientation = getCurrentActivity().getResources().getConfiguration().orientation;
+        String orientationStr = orientation == ORIENTATION_LANDSCAPE ? "landscape" : "portrait";
+        promise.resolve(orientationStr);
+    }
+}
